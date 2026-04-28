e0fd7d4e9ad006871c4c4f8c87694cbe3de67981
will <willsandersnpt@gmail.com>
2026-04-28 14:17:27 -0400

    Added dependencies and updated gitignore

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from appsoftcomp-finalproject!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ notebook.py
@@ -0,0 +1,34 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "marimo>=0.23.4",
+#     "matplotlib==3.10.9",
+#     "numpy==2.4.4",
+#     "pandas==3.0.2",
+# ]
+# ///
+
+import marimo
+
+__generated_with = "0.23.4"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import pandas as pd
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
